f070462d82236fc156b45b167a0a886b08bcfdb2
LN <luanmf13@gmail.com>
2025-04-10 20:11:18 -0300

    Classes leitos

---
+++ libsystem/src/main/java/lmp/libsystem/reader.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lmp.libsystem;
+
+/**
+ *
+ * @author LuanM
+ */
+public class reader {
+    
+    private String readerName;
+    
+    private boolean readerGenre;
+    
+    private boolean readerNeedsReturnBook;
+    
+    private int readerAge;
+    
+    private int totalReaderLentBooks;
+    
+    
+    
+}
