a97d97b6b082dc3f0a84a2458b8c6a0d6f5fa5fe
LN <luanmf13@gmail.com>
2025-04-11 19:15:27 -0300

    1

--- libsystem/src/main/java/lmp/libsystem/Libsystem.java
+++ libsystem/src/main/java/lmp/libsystem/Libsystem.java
@@ -1,7 +1,12 @@
 
 
 package lmp.libsystem;
+import java.util.Scanner;
 import  lmp.libsystem.books;
+import lmp.libsystem.reader;
+
+
 public class Libsystem {
-    
+    Scanner scanner = new Scanner (System.in);
+
 }
--- libsystem/src/main/java/lmp/libsystem/books.java
+++ libsystem/src/main/java/lmp/libsystem/books.java
@@ -30,85 +30,8 @@ public class books {
     
     private boolean isBookSerie;
 
-    public void setBookId(Long bookId) {
-        this.bookId = bookId;
-    }
 
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public void setBookSeries(String bookSeries) {
-        this.bookSeries = bookSeries;
-    }
-
-    public void setBookAuthor(String bookAuthor) {
-        this.bookAuthor = bookAuthor;
-    }
-
-    public void setBookType(int bookType) {
-        this.bookType = bookType;
-    }
-
-    public void setBookGenre(int bookGenre) {
-        this.bookGenre = bookGenre;
-    }
-
-    public void setBookQuantity(int bookQuantity) {
-        this.bookQuantity = bookQuantity;
-    }
-
-    public void setBookReserverd(boolean bookReserverd) {
-        this.bookReserverd = bookReserverd;
-    }
-
-    public void setBookMissing(boolean bookMissing) {
-        this.bookMissing = bookMissing;
-    }
-
-    public void setIsBookSerie(boolean isBookSerie) {
-        this.isBookSerie = isBookSerie;
-    }
-
-    public Long getBookId() {
-        return bookId;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public String getBookSeries() {
-        return bookSeries;
-    }
-
-    public String getBookAuthor() {
-        return bookAuthor;
-    }
-
-    public int getBookType() {
-        return bookType;
-    }
-
-    public int getBookGenre() {
-        return bookGenre;
-    }
-
-    public int getBookQuantity() {
-        return bookQuantity;
-    }
-
-    public boolean isBookReserverd() {
-        return bookReserverd;
-    }
-
-    public boolean isBookMissing() {
-        return bookMissing;
-    }
-
-    public boolean isIsBookSerie() {
-        return isBookSerie;
-    }
+    
     
     
 }
