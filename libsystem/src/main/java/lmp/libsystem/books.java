f652e7374617b3d72ca7b2b386fd20cdc27649fc
LN <luanmf13@gmail.com>
2025-04-12 00:05:55 -0300

    Update books.java

--- libsystem/src/main/java/lmp/libsystem/books.java
+++ libsystem/src/main/java/lmp/libsystem/books.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package lmp.libsystem;
 
-/**
- *
- * @author LuanM
- */
+
 public class books {
     
     private Long bookId;
